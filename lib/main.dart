9585c068958d463e21b582422dd90f35f69e82d4
Shaurya Thakur <176936155+Shaurya0987@users.noreply.github.com>
2026-01-22 12:13:03 +0530

    Product Model Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebaselogindemo/Screens/LoginScreen.dart';
 import 'package:firebaselogindemo/Screens/OperationScreen.dart';
-import 'package:firebaselogindemo/Screens/ProfileGate.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
